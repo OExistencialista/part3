3c2efc6fc73f2ab73d51e5c9cffef5183e25aade
OExistencialista <134657253+OExistencialista@users.noreply.github.com>
2024-06-27 10:17:47 -0300

    Add files via upload

---
+++ part3/problema6/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program{
+    static void Main(){
+        Console.Write("taxa: ");
+        double taxa = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Valor Inicial: ");
+        double valorInicial = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Periodo: ");
+        double periodo = Convert.ToDouble(Console.ReadLine());
+        double valorFinal = valorInicial * Math.Pow((1+taxa/100), periodo);
+        Console.Clear();
+        Console.WriteLine($"Valor Final : {valorFinal:F2}");
+        Console.WriteLine($"Taxa: {taxa:F2}");
+        Console.WriteLine($"Valor Inicial : {valorInicial:F2}");
+        Console.WriteLine($"Periodo : {periodo:F2}");
+    }
+}
---
+++ part3/problema7e8/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("taxa: ");
+        double taxa = Convert.ToDouble(Console.ReadLine())/100;
+        Console.Write("Valor Inicial: ");
+        double valorInicial = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Valor do saque: ");
+        double saque = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Periodo: ");
+        int periodo = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Digite a date do saque se existir: ");
+        int mesRegaste  = Convert.ToInt32(Console.ReadLine());
+
+        
+        
+        
+        for (int i = 1; i <= periodo; i++)
+        {
+            valorInicial = juroscompostos(valorInicial, taxa, 1);
+            double mensal =valorInicial - juroscompostos(valorInicial, -taxa, 1);
+            if(i == mesRegaste){ valorInicial-= saque;
+            
+            }
+
+
+            Console.WriteLine(
+            $" {i} |".PadLeft(7, ' ') +
+            $"{mensal:F2} |".PadLeft(19, ' ') +
+            $"{valorInicial:F2} | ".PadLeft(19, ' ') +((i==mesRegaste)? $"resgate: -{saque}": "")
+             );
+             
+        }
+
+        static double juroscompostos(double valor, double taxa, int periodo){
+            return valor * Math.Pow(taxa+1, periodo);
+        }
+    }
+}
